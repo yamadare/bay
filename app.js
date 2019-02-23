3bde1d291bd28b3ab4f6b6972c737dfa4fcda8c7
Yosuke Sakamura <y-sakamura@digital-sphere.co.jp>
2019-02-23 13:11:52 +0900

    add memberpage

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Routing
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const memberRouter = require('./routes/member')
 
 var app = express();
 
@@ -20,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/member', memberRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
