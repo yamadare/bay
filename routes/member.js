a96f3f0f767db75866cfa93c4dfb957b13dcda4e
Yosuke Sakamura <y-sakamura@digital-sphere.co.jp>
2019-03-03 16:26:29 +0900

    edit server

--- routes/member.js
+++ routes/member.js
@@ -1,46 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const member = [
-  {
-    id: 1,
-    name: '坂村 洋輔',
-    age: 24,
-    image: '/images/member/sakamura.PNG',
-    instrument: 'tenor sax'
-  },
-  {
-    id: 2,
-    name: '坂村 洋輔',
-    age: 24,
-    image: '/images/member/sakamura.PNG',
-    instrument: 'tenor sax'
-  },
-  {
-    id: 3,
-    name: '坂村 洋輔',
-    age: 24,
-    image: '/images/member/sakamura.PNG',
-    instrument: 'tenor sax'
-  },
-  {
-    id: 4,
-    name: '坂村 洋輔',
-    age: 24,
-    image: '/images/member/sakamura.PNG',
-    instrument: 'tenor sax'
-  },
-  {
-    id: 5,
-    name: '坂村 洋輔',
-    age: 24,
-    image: '/images/member/sakamura.PNG',
-    instrument: 'tenor sax'
-  },
-]
+router.get('/', (req,res,next) => {
+  res.send('member desc')
+})
 
-router.get('/', (req, res, next) => {
-  res.render('member', {member: member})
-});
 
 module.exports = router;
